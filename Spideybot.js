b056321e2e84ddeef823ee8c0badc422f8e0c66c
sith202 <44218400+sith202@users.noreply.github.com>
2020-12-08 13:10:04 -0600

    Update Spideybot.js

--- Spideybot.js
+++ Spideybot.js
@@ -1,44 +1,23 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+// login to Discord with your app's token
+client.login('NTAyMTkxMDM4MDI3MjY4MTAw.W8eD2Q.8FpvHFI2fRHS-mo8YjF-DXAImbY');
+
+client.on('message', message => {
+	console.log(message.content);
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            case 'Hello':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Greetings!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+if (message.content === '!ping') {
+	// send back "Pong." to the channel the message was sent in
+	message.channel.send('Pong.');
+}
